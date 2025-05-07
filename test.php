0ac7c1994eaefaec4f901a8d01db0084e7852223
Adrian3312401110 <adrianrizqullah4@gmail.com>
2025-05-07 10:48:13 +0700

    modif test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - My Laravel App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container py-5">
+        <h1>Selamat Datang di Halaman test</h1>
+        <p>Ini adalah halaman utama dari aplikasi Laravel Anda.</p>
+        <a href="{{ route('contact') }}" class="btn btn-primary">Hubungi Kami</a>
+    </div>
+</body>
+</html>
